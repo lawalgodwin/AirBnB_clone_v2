80e296e4cd68ede34776a4382a2548dca6bad57c
lawalgodwin <godwinl200@gmail.com>
2023-04-08 11:20:40 +0100

    Install and configure nginx to serve some content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # A Bash script that sets up your web servers for the deployment of web_static
-
 # Install Nginx if it not already installed
 # Create the folder /data/ if it doesn’t already exist
 # Create the folder /data/web_static/ if it doesn’t already exist
@@ -8,11 +7,10 @@
 # Create the folder /data/web_static/shared/ if it doesn’t already exist
 # Create the folder /data/web_static/releases/test/ if it doesn’t already exist
 # Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-#	Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
-#	Use alias inside your Nginx configuration
-
+# Use alias inside your Nginx configuration
 # install and configure nginx to serve some content
 if ! which nginx > /dev/null 2>&1; then
   sudo apt-get update
