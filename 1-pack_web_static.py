e3a1bf944282e02d9e091f068cfcc67b5a2dd608
lawalgodwin <godwinl200@gmail.com>
2023-04-15 16:02:31 +0100

    Compress files before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""
-A module that compresses files before sending
-   All files in the folder web_static must be added to the final archive
-   Store all archives in the folder versions(create if it doesn’t exist)
-   Create archives like web_static_<year><month><day><hour><minute><second>.tgz
-   The function do_pack must return the archive path if the
-   archive has been correctly generated. Otherwise, it should return None
-"""
-from fabric.api import local, task
-from datetime import datetime
+from fabric.api import local
+from time import strftime
+from datetime import date
 
 
-@task
 def do_pack():
-    """A function that compresses the files before sending"""
+    """ A script that generates archive the contents of web_static folder"""
 
-    now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour
-    minute = now.minute
-    second = now.second
-    archive_name = f"web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
-    local('mkdir -p versions')
+    filename = strftime("%Y%m%d%H%M%S")
     try:
-        local(f'tar -cvzf versions/{archive_name} web_static')
-        return f'versions/{archive_name}'
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static"
+              .format(filename))
+        return "versions/web_static_{}.tgz".format(filename)
     except Exception as e:
         return None
