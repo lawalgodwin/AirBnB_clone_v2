0165594a80540ddd1c84611dc3682aa4255ddb08
lawalgodwin <godwinl200@gmail.com>
2023-04-15 16:28:21 +0100

    Compress files before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,12 +8,6 @@ def do_pack():
     """ A function that compresses the files before sending"""
 
     now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour
-    minute = now.minute
-    second = now.second
     archive_name = f"web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
     local('mkdir -p versions')
     try:
