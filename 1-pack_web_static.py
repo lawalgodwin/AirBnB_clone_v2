bd167084e9e6eab4876a95914697eb1bc3c98eaf
lawalgodwin <godwinl200@gmail.com>
2023-04-15 16:31:23 +0100

    Compress files before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,3 +8,9 @@ def do_pack():
     """ A function that compresses the files before sending"""
 
     now = datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    second = now.second
