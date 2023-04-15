27ee10249bba11a4a5ea10ae2dc8e5e3a06dd934
lawalgodwin <godwinl200@gmail.com>
2023-04-15 16:16:56 +0100

    Compress files before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,18 +6,3 @@ from datetime import datetime
 
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
-
-    now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour
-    minute = now.minute
-    second = now.second
-    archive_name = f"web_static_{year}{month}{day}{hour}{minute}{second}.tgz"
-    local('mkdir -p versions')
-    try:
-        local(f'tar -cvzf versions/{archive_name} web_static')
-        return f'versions/{archive_name}'
-    except Exception as e:
-        return None
