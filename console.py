9e0f38c02ffe5b8205e6b9e4e216e38e467b8616
lawalgodwin <godwinl200@gmail.com>
2023-06-20 13:49:56 +0100

    Edit the quit command

--- console.py
+++ console.py
@@ -94,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
-        exit()
+            ptint("Exiting...")
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
