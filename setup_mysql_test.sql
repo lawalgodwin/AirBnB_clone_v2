d43d11320297a2430b13a21706ede2822a5e9bc7
lawalgodwin <godwinl200@gmail.com>
2023-06-21 12:14:50 +0100

    Add mysql sever setup script for test environment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 -- A script that prepares a MySQL server for the project
 
 -- create new db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- create a user in the localhost
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
