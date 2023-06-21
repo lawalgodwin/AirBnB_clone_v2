b2149b3b550222a761a3d31ed143c19d52f6aa56
lawalgodwin <godwinl200@gmail.com>
2023-06-21 11:19:00 +0100

    Add mysql sever setup script for test environment

---
+++ setup_mysql_test.sql
@@ -0,0 +1,16 @@
+-- MySQL setup for test environment
+-- A script that prepares a MySQL server for the project
+
+-- create new db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create a user in the localhost
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant all privileges to the created user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant only read privilege on db 'performance_schema' to the created user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Let all modifications take effect immediately
+FLUSH PRIVILEGES;
